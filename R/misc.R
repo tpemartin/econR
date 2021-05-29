f9171480b7b29c38a38afaea48d4a4006c269d14
tpemartin <tpemartin@gmail.com>
2021-05-29 19:20:16 +0800

    add google share link download

--- R/misc.R
+++ R/misc.R
@@ -103,3 +103,31 @@ convert2script <- function(){
     scriptfilepath
   )
 }
+#' Download Google drive shared link to a destination folder
+#'
+#' @param googleSharedLink A shared link
+#' @param destfolder A destination folder path
+#'
+#' @return
+#' @export
+#'
+#' @examples none
+googleLink_download <- function(googleSharedLink,
+  destfolder=destfolder){
+  googledrive::as_dribble(googleSharedLink) -> drb
+  googledrive::drive_ls(drb) -> allFiles
+  if(!dir.exists(destfolder)) dir.create(destfolder)
+
+  purrr::walk(
+    1:nrow(allFiles),
+    ~{
+      fileX = allFiles[.x, ]
+      googledrive::drive_download(
+        file = allFiles[.x, ],
+        path = file.path(destfolder, fileX$name),
+        overwrite = T
+      )
+    }
+  )
+
+}
